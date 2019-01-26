55f925c0f1d4682d0639b1a75d76eb3940e35f37
Hsing-Yukai <std_ethanfrank@outlook.com>
2019-01-26 23:00:29 +0800

    update TimerPool_UnitTest

--- UnitTest/TimerPool_UnitTest.cpp
+++ UnitTest/TimerPool_UnitTest.cpp
@@ -2,3 +2,14 @@
 // Created by kiwi on 19-1-21.
 //
 
+#include "../Base/TimerPool.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Kiwi::TimerPool::TimerList a;
+	cout<<sizeof(a);
+	return 0;
+}
