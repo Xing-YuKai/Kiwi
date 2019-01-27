61c5e02d70ba5012de7db167a0e0956af9a4672b
Hsing-Yukai <std_ethanfrank@outlook.com>
2019-01-27 17:03:37 +0800

    update TimerPool_UnitTest

--- UnitTest/TimerPool_UnitTest.cpp
+++ UnitTest/TimerPool_UnitTest.cpp
@@ -9,7 +9,36 @@ using namespace std;
 
 int main()
 {
-	Kiwi::TimerPool::TimerList a;
-	cout<<sizeof(a);
+	Kiwi::TimerPool pool;
+	Kiwi::TimeRange nowtime;
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(2s, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer1" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+	pool.update();
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(1s, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer2" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+	pool.update();
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(300ms, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer3" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(600ms, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer4" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+	pool.update();
 	return 0;
 }
