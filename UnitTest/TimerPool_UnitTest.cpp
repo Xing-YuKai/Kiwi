0fc177d759be19a4cfd585f71aee682adf0671c0
Hsing-Yukai <std_ethanfrank@outlook.com>
2019-01-27 18:09:45 +0800

    update TimerPool_UnitTest

--- UnitTest/TimerPool_UnitTest.cpp
+++ UnitTest/TimerPool_UnitTest.cpp
@@ -11,34 +11,68 @@ int main()
 {
 	Kiwi::TimerPool pool;
 	Kiwi::TimeRange nowtime;
-	nowtime = Kiwi::TimeRange::now();
-	pool.start_timer(2s, [nowtime]
-	{
-		Kiwi::TimeRange current = Kiwi::TimeRange::now();
-		std::cout << "Timer1" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
-	});
-	pool.update();
+
+	Kiwi::TimeRange time1 = Kiwi::TimeRange::now();
 
 	nowtime = Kiwi::TimeRange::now();
 	pool.start_timer(1s, [nowtime]
 	{
 		Kiwi::TimeRange current = Kiwi::TimeRange::now();
-		std::cout << "Timer2" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+		std::cout << "Timer1 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
 	});
-	pool.update();
 
 	nowtime = Kiwi::TimeRange::now();
-	pool.start_timer(300ms, [nowtime]
+	pool.start_timer(2s, [nowtime]
 	{
 		Kiwi::TimeRange current = Kiwi::TimeRange::now();
-		std::cout << "Timer3" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+		std::cout << "Timer2 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
 	});
+
 	nowtime = Kiwi::TimeRange::now();
-	pool.start_timer(600ms, [nowtime]
+	pool.start_timer(3s, [nowtime]
 	{
 		Kiwi::TimeRange current = Kiwi::TimeRange::now();
-		std::cout << "Timer4" << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+		std::cout << "Timer3 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
 	});
-	pool.update();
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(4s, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer4 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(100ms, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer5 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(90ms, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer6 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+
+	nowtime = Kiwi::TimeRange::now();
+	pool.start_timer(80ms, [nowtime]
+	{
+		Kiwi::TimeRange current = Kiwi::TimeRange::now();
+		std::cout << "Timer7 " << current.get_micro_second_units() - nowtime.get_micro_second_units() << "us" << std::endl;
+	});
+
+	Kiwi::TimerID timer8 = pool.start_timer(200ms, [nowtime]
+	{
+		std::cout << "Timer8 stop fail " << std::endl;
+	});
+	pool.stop_timer(timer8);
+
+	while(true)
+	{
+		pool.update();
+	}
+
 	return 0;
 }
