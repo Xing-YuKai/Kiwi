4e8820f11990fb3b68666bf18eab181f3c1e9d2d
Hsing-Yukai <std_ethanfrank@outlook.com>
2019-02-02 00:55:00 +0800

    git update EchoServer

--- example_src/EchoServer.cpp
+++ example_src/EchoServer.cpp
@@ -3,13 +3,36 @@
 //
 
 #include "../Kiwi/Network/TcpServer.h"
+#include "../Kiwi/Network/TcpConnection.h"
 #include "../Kiwi/Base/InetAddress.h"
+#include "../Kiwi/Reactor/EventLoop.h"
+
+void connection_handler(const Kiwi::Type::TcpConnectionPtr &conn_ptr)
+{
+	std::string data("EchoServer");
+	conn_ptr->send(data);
+}
+
+void message_handler(const Kiwi::Type::TcpConnectionPtr &conn_ptr,
+					 const Kiwi::Type::BufferPtr &buffer,
+					 const Kiwi::TimeRange &receive)
+{
+	std::string data;
+	data = buffer->retrieve(buffer->get_readable_bytes());
+	conn_ptr->send(data);
+}
 
 int main()
 {
 	Kiwi::InetAddress listen_address;
+	Kiwi::EventLoop base_loop;
 	listen_address.set_address_any();
-	listen_address.set_port(3333);
-	Kiwi::TcpServer server(4,listen_address);
+	listen_address.set_port(23333);
+
+	Kiwi::TcpServer server(&base_loop, 4, listen_address);
+	server.set_message_handler(message_handler);
+	server.set_connection_handler(connection_handler);
+	server.run();
+	std::cout << "des" << std::endl;
 	return 0;
 }
