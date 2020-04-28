c283cefdb31df2a30d7f9041fdd426ee6c063ba7
xingyukai.ethan <xingyukai.ethan@bytedance.com>
2020-04-28 23:53:26 +0800

    update

--- Kiwi/build.sh
+++ Kiwi/build.sh
@@ -1,7 +1,13 @@
+rm -rf ../lib_release
 rm -rf build
 mkdir build
 cd build
 cmake ..
 make
 cd ..
+
+mkdir -p ../lib_release/header/Base
+mkdir -p ../lib_release/header/Network
+mkdir -p ../lib_release/header/Reactor
+mkdir -p ../lib_release/header/ThreadPool
 for i in */*.h;do cp $i ../lib_release/header/$i; done;
